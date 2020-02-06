f3fbbe99fcabb4e25c5a5d34ee6211807eeefe93
Scott Feier <scott.feier@fakemail.com>
2020-02-06 15:33:59 -0500

    initial segue work

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -9,21 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var swoosh: UIImageView!
-
     @IBOutlet weak var bgImg: UIImageView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // manual way to center swoosh logo in the frame
-
+    }
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
-
+    
 
 }
 
