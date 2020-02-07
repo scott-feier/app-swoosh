252fc7f8959f6d826d3eb5174ccf0cbd8f0e6ff2
Scott Feier <scott.feier@fakemail.com>
2020-02-07 13:08:58 -0500

    pass variables on segue - end of app

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -48,7 +48,7 @@ class LeagueVC: UIViewController {
     
     
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
-        player = nil
+        player.desiredLeague = nil
         nextBtn.isEnabled = false
      }
     
@@ -57,10 +57,13 @@ class LeagueVC: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
-    */
 
 }
